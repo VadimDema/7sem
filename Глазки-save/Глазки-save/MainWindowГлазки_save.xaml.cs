3444101efa7916b24321a99e4d56fba24733241b
VadimDema <90608445+VadimDema@users.noreply.github.com>
2022-10-27 12:15:41 +0300

    Add files via upload

---
+++ Глазки-save/Глазки-save/MainWindowГлазки_save.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Глазки_save
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowГлазки_save : Window
+    {
+        public MainWindowГлазки_save()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new User());
+        }
+        private void Btn_admin(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Btn_user(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+
+    
